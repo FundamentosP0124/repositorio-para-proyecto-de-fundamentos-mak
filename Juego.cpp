3ec12052595c1d51cca1ea72bc1bf4a2a7b26781
Katherine024z <164245475+Katherine024z@users.noreply.github.com>
2024-06-25 17:11:13 -0600

    Add files via upload

--- Juego.cpp
+++ Juego.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+//Funciones
 void instruccion(void){
-    cout << "Este es un juego de EQUIS-CERO\nConsiste en poner una X o un 0 de forma horizontal, diagonal o vertical"; 
+    cout << "Este es un juego de EQUIS-CERO\nConsiste en poner una X o un 0 de forma horizontal, diagonal o vertical"; ;
+}
+void ayuda(void){
+    cout << "¿Como jugar?\n";
+    cout << "EQUIS-CERO consiste en colocar X o 0 de manera que formen una fila, columna o diagonal\n";
+    cout << "Gana quien complete primero una de estas tres formas\n";
+    cout << "Seran 3 partidas";
+}
+void creadoras(void){
+    cout << "Este es un juego creado por:\n Monica Diaz\n Andrea Marquez\n Katherine Martinez";
 }
 void ayuda(void){
     cout << "EQUIS-CERO consiste en colocar X o 0 de manera que formen una fila, columna o diagonal\nGana quien complete primero una de estas tres formas";
@@ -13,21 +23,32 @@ void opcion(void){
 
 int main()
 {
-    int numero;
+    int respuesta;
+    string opcion, si, SI, no, NO;
     cout << "JUEGO DE EQUIS-CERO";
-    cout << "1. Jugar\n2. Ayuda\n3.Opciones";
+    cout << "\n1. Jugar\n2. Ayuda\n3.Creadoras\n";
+    cout << "(Selecciona una opcion)\n";
+    cin >> respuesta;
 
-    switch (numero)
+    switch (respuesta)
     {
         case 1:
-        instruccion;
+        instruccion();
+        cout << endl;
+        cout << "Desea ver *como jugar?* antes de continuar?\n   SI   NO\n";
+        cin >> opcion;
+        if (opcion == si||opcion == SI)
+        {
+            ayuda();
+        }
+        break;
 
         case 2:
-        ayuda;
+        ayuda();
+        break;
 
         case 3:
-        opcion;
-
+        creadoras();
+        break;
     }
-    
 }
