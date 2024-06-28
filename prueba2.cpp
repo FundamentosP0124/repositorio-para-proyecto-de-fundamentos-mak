d271dc25f2e63b29dd9eb160f603ff44244e7e81
monicadiaz51 <160180802+monicadiaz51@users.noreply.github.com>
2024-06-28 11:28:28 -0600

    Update prueba2.cpp

--- prueba2.cpp
+++ prueba2.cpp
@@ -1,88 +1,86 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
-char fila;
-char columna;
-char empate;
+// Constantes para el tamaño del tablero y los jugadores
+const int TAMANO = 3;
+const char JUGADOR_X = 'X';
+const char JUGADOR_O = 'O';
+const char VACIO = ' ';
 
-bool turnoDeX = false;
-
-char tablero[][3]= { {'_','_','_'},
-                     {'_','_','_'},
-                     {'_','_','_'}
-                    
-};
-
-char hayGanador(){
-    //validacion horizontal
-    for(int i= 0; i<3;i++){
-
-        if(tablero[i][0] != '_' && tablero [i][0] == tablero[i][1] && tablero [i][1]  == tablero[i][2] ){
-        return tablero[i][0];
-
+// Función para inicializar el tablero vacío
+void inicializarTablero(char tablero[TAMANO][TAMANO]) {
+    for (int i = 0; i < TAMANO; ++i) {
+        for (int j = 0; j < TAMANO; ++j) {
+            tablero[i][j] = VACIO;
+        }
     }
-    
+}
+
+// Función para mostrar el tablero
+void mostrarTablero(char tablero[TAMANO][TAMANO]) {
+    for (int i = 0; i < TAMANO; ++i) {
+        for (int j = 0; j < TAMANO; ++j) {
+            cout << tablero[i][j];
+            if (j < TAMANO - 1) cout << " | ";
+        }
+        cout << endl;
+        if (i < TAMANO - 1) cout << "--+---+--" << endl;
+    }
+}
+
+// Función para verificar si hay un ganador
+char verificarGanador(char tablero[TAMANO][TAMANO]) {
+    // Verificar filas
+    for (int i = 0; i < TAMANO; ++i) {
+        if (tablero[i][0] != VACIO && tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2]) {
+            return tablero[i][0];
+        }
     }
 
-    //validacion vertical
-    for(int i= 0; i<3;i++){
+    // Verificar columnas
+    for (int i = 0; i < TAMANO; ++i) {
+        if (tablero[0][i] != VACIO && tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]) {
+            return tablero[0][i];
+        }
+    }
 
-        if(tablero[0][i] != '_' && tablero [0][i] == tablero[1][i] && tablero [1][i]  == tablero[2][i] ){
-        return tablero[0][i];
-    }
-    }
-    //validacion diagonal de izquierda a derecha
-     if(tablero[0][0] != '_' && tablero [0][0] == tablero[1][1] && tablero [1][1]  == tablero[2][2] ){
+    // Verificar diagonales
+    if (tablero[0][0] != VACIO && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) {
         return tablero[0][0];
     }
-    //validacion diagonal derecha a izquierda
-     if(tablero[0][2] != '_' && tablero [0][2] == tablero[1][1] && tablero [1][1]  == tablero[2][0] ){
+    if (tablero[0][2] != VACIO && tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]) {
         return tablero[0][2];
     }
 
-} 
-
-bool hayEmpate(){
-    for(int i= 0; i<3; i++){
-        for(int j = 0; j<3; j++){
-           if(tablero[i][j]=='_'){
-            return false;
-           }
-        }
-        
-    }
-    return true;
-
+    // No hay ganador aún
+    return VACIO;
 }
 
-void mostrarTablero(){
-    char jugador = turnoDeX ? 'X' : '0';
-    for(int i= 0; i<3; i++){
-        for(int j = 0; j<3; j++){
-            cout <<tablero[i][j] <<"\n";
-        }
-        cout<< endl;
+// Función para calcular el puntaje
+int calcularPuntaje(char tablero[TAMANO][TAMANO], char jugador) {
+    char ganador = verificarGanador(tablero);
+    if (ganador == jugador) {
+        return 10;  // Ganar
+    } else if (ganador != VACIO && ganador != jugador) {
+        return -10;  // Perder
+    } else {
+        return 0;  // Empate o juego en curso
     }
+}
 
-    cout<<"es el turno de "<< jugador << endl;
-    cout<<"en que fila desea jugar: ";
-    cin>> fila;
-    cout<<"en que columna desea jugar: ";
-    cin>> columna;
-    tablero[fila][columna] = jugador;
-    turnoDeX = !turnoDeX;
-    char ganador = hayGanador();
-    if(ganador != '_'){
-        cout<<" ha ganado"<< ganador << endl;
-        exit(0);
-    }
-    if(hayEmpate()){
-        cout<<"hay empate"<< empate <<endl;
-        exit(0);
-    }
+int main() {
+    char tablero[TAMANO][TAMANO];
+    inicializarTablero(tablero);
+    mostrarTablero(tablero);
+
+    // Calcular y mostrar el puntaje
+    int puntaje = calcularPuntaje(tablero, JUGADOR_X);
+    cout << "Puntaje para X: " << puntaje << "\n";
+   
+    cout<< "puntaje para 0: " << puntaje << "\n";
+
+    return 0;
 }
-int main(int argc, char **argv){
-    while(true)
-    mostrarTablero();
-}
+
+    
+
