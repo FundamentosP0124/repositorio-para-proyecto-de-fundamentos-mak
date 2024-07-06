f2668f21ae4914dbff88699fe43201ad7a3a2af4
Katherine024z <164245475+Katherine024z@users.noreply.github.com>
2024-07-05 21:23:11 -0600

    Mejora de las instrucciones de juego

--- Juego.cpp
+++ Juego.cpp
@@ -4,13 +4,18 @@ using namespace std;
 //Funciones
 void instruccion(void){
     cout << "Este es un juego de EQUIS-CERO (o tambien llamado Tic Tac Toe)\n";
-    cout << "Consiste en poner una X o un 0 de forma horizontal, diagonal o vertical" ;
+    cout << "Consiste en poner una X o un 0 de forma horizontal, diagonal o vertical\n";
 }
 void ayuda(void){
     cout << "¿Como jugar?\n";
     cout << "EQUIS-CERO consiste en colocar X o 0 de manera que formen una fila, columna o diagonal\n";
     cout << "Gana quien complete primero una de estas tres formas\n";
+    cout << "Para jugar, tiene que ingresar su nombre de jugador y entre parentesis se le indicara que tipo de opcion es (x) o (0)\n";
+    cout << "Luego de ingresar los datos, se mostrara en pantalla el turno del jugador (EJ: NOMBREJUGADOR (X)... )\n";
+    cout << "Posterior a eso, tiene que ingresar el numero de la casilla donde desea colocar su opcion\n";
+    cout << "Despues de colocar la opcion, le toca el turno al siguiente jugador y asi sucesivamente\n";
     cout << "Seran 3 partidas";
+
 }
 void creadoras(void){
     cout << "Este es un juego creado por:\n Monica Diaz 00165224\n Andrea Marquez 00033924\n Katherine Martinez 00045724\n";
@@ -29,11 +34,12 @@ int main()
     {
         case 1:
         instruccion();
-        cout << "\nDesea ver *como jugar?* antes de comenzar?\n  SI   NO\n";
+        cout << "Desea ver *como jugar?* antes de comenzar?\n";
         cin >> opcion;
         cout << "\n";
         if (opcion == si || opcion == SI)
         {
+            cout << endl;
             ayuda();
         }
     
