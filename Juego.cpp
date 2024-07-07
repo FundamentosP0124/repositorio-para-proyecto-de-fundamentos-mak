2536d9a9ecd9559c6c5777688f1a5bf830dd000e
Katherine024z <164245475+Katherine024z@users.noreply.github.com>
2024-07-06 21:07:37 -0600

    Mejora de instrucciones

--- Juego.cpp
+++ Juego.cpp
@@ -7,15 +7,15 @@ void instruccion(void){
     cout << "Consiste en poner una X o un 0 de forma horizontal, diagonal o vertical\n";
 }
 void ayuda(void){
-    cout << "¿Como jugar?\n";
+     cout << "¿Como jugar?\n";
     cout << "EQUIS-CERO consiste en colocar X o 0 de manera que formen una fila, columna o diagonal\n";
     cout << "Gana quien complete primero una de estas tres formas\n";
     cout << "Para jugar, tiene que ingresar su nombre de jugador y entre parentesis se le indicara que tipo de opcion es (x) o (0)\n";
     cout << "Luego de ingresar los datos, se mostrara en pantalla el turno del jugador (EJ: NOMBREJUGADOR (X)... )\n";
     cout << "Posterior a eso, tiene que ingresar el numero de la casilla donde desea colocar su opcion\n";
     cout << "Despues de colocar la opcion, le toca el turno al siguiente jugador y asi sucesivamente\n";
-    cout << "Seran 3 partidas";
-
+    cout << "Seran 3 partidas\n";
+    cout << endl;
 }
 void creadoras(void){
     cout << "Este es un juego creado por:\n Monica Diaz 00165224\n Andrea Marquez 00033924\n Katherine Martinez 00045724\n";
@@ -26,8 +26,8 @@ int main()
     int respuesta;
     cout << "JUEGO DE EQUIS-CERO";
     cout << "\n1. Jugar\n2. Ayuda\n3.Creadoras\n";
-    cout << "Si usted quiere empezar directamente con la seccion *ayuda* seleccione la opcion 2\n";
     cout << "(Selecciona una opcion)\n";
+    cout << "**Si usted quiere tener indicaciones mas detalladas seleccione *Ayuda*\n";
     cin >> respuesta;
 
     switch (respuesta)
